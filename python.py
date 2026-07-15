a877a663e695094531cbc0adc41aaf2cb52069f9
janhavilive02 <janhavi.live02@gmail.com>
2026-07-15 22:35:12 +0530

    Add multiple functions and control structures

--- python.py
+++ python.py
@@ -3,3 +3,42 @@ def func_name(name,age):
 
 func_name("Mike",12)
 func_name("Steve",13)
+
+'''def cube(num):
+    return num*num*num
+print(cube(3))'''
+
+'''is_male = False
+is_tall = True 
+if is_male or is_tall:
+    print("You are a male and tall")
+elif is_male and not(is_tall):
+    print("You atre a short male")
+else:
+    print("You are not a male not tall")'''
+
+'''def max_num(num1,num2,num3):
+    if num1>=num2 and num1>=num3:
+        return num1
+    elif num2>=num1 and num2>=num3:
+        return num2
+    else:
+        return num3
+print(max_num(1,20,3))'''
+
+#calc
+'''num1 = float(input("Enter the first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1+num2)
+elif op == "-":
+    print(num1-num2)
+elif op == "*":
+    print(num1*num2)
+else: 
+    print(num1/num2)'''
+
+#dictionary
+num1 = 3
