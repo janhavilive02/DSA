1f4b8458eafc7167540492c8130bc9c037b7b675
janhavilive02 <janhavi.live02@gmail.com>
2026-07-17 16:26:19 +0530

    while

--- python.py
+++ python.py
@@ -52,3 +52,50 @@ else:
 
 #print(monthCoverstions["Apr"])
 print(monthCoverstions.get("Dec", "Not a valid key"))'''
+
+#while loop - moves through the specific statement given number of times
+'''i = 1
+while i <= 10:
+    print(i)
+    i += 1 
+print("done with loop")'''
+
+#guessing game 
+'''secret_word = "giraffe"
+guess = ""
+guess_count = 0 
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    # for answer
+    if guess_count < guess_limit: 
+        guess = input("Enter guess: ")
+        guess_count+=1
+    else: 
+        out_of_guesses = True
+
+if out_of_guesses: 
+    print("You are out of guesses!")
+else:
+    print("You win!")
+
+
+secret_word = "Apple"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter guess: ")
+        guess_count += 1
+    else: 
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("You are out of guesses!")
+else:
+    print("You win!")
+    '''
